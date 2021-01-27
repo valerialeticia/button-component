7d5f637179897d4d2d719afc686a27a481680288
valerialeticia <valerialeticia965@gmail.com>
2021-01-26 21:54:09 -0300

    sent files

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-main>
+        <h2>Buttons</h2>
+
+        <v-row class="my-5">
+          <v-col cols="12" lg="4" md="4">
+            <Button label="Default" />
+          </v-col>
+        </v-row>
+
+        <v-row class="my-5">
+          <v-col cols="12" lg="4" md="4">
+            <Button label="Default" :outlined="true" color="indigo accent-4" />
+          </v-col>
+        </v-row>
+
+        <v-row class="my-5">
+          <v-col cols="12" lg="4" md="4">
+            <Button label="Default" :text="true" color="indigo accent-4" />
+          </v-col>
+        </v-row>
+
+      <v-row class="my-5">
+        <v-col cols="12" lg="4" md="4">
+          <Button label="Default" elevation="0" color="indigo accent-4" :dark="true" />
+        </v-col>
+      </v-row>
+
+      <v-row class="my-5">
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default"  :disabled="true" />
+        </v-col>
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default"  :disabled="true" :text="true" />
+        </v-col>
+      </v-row>
+
+      <v-row class="my-5">
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default" color="indigo accent-4"  iconLeft="mdi-cart-plus" :dark="true" />
+        </v-col>
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default" color="indigo accent-4"  iconRight="mdi-cart-plus" :dark="true" />
+        </v-col>
+      </v-row>
+
+      <v-row class="my-5">
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default" color="indigo accent-4" :dark="true" :small="true" />
+        </v-col>
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default" color="indigo accent-4"  :dark="true" />
+        </v-col>
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default" color="indigo accent-4" :dark="true" :large="true" />
+        </v-col>
+      </v-row>
+
+      <v-row class="my-5">
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default" color="grey"  />
+        </v-col>
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Default" color="indigo accent-4"  :dark="true" />
+        </v-col>
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Secondary" color="blue-grey darken-3" :dark="true" />
+        </v-col>
+        <v-col cols="12" lg="2" md="2">
+          <Button label="Danger" color="red darken-4" :dark="true" />
+        </v-col>
+      </v-row>
+      </v-main>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import Button from './components/Button'
+export default {
+  name: 'Buttons',
+
+  components: {
+    Button
+  }
+}
+</script>
---
+++ src/components/Button.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <v-btn 
+            :outlined="outlined" 
+            :disabled="disabled" 
+            :text="text"
+            :color="color"
+            :elevation="elevation"
+            :dark="dark"
+            :small="small"
+            :x-large="large"
+            class="text-capitalize rounded-lg">
+            <v-icon class="mr-2">{{ iconLeft }}</v-icon>
+            {{ label }}
+            <v-icon class="ml-2">{{ iconRight }}</v-icon>
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            default: ''
+        },
+        iconRight: {
+            type: String,
+            default: ''
+        },
+        iconLeft: {
+            type: String,
+            default: ''
+        },
+        outlined: {
+            type: Boolean,
+            default: false
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        text: {
+           type: Boolean,
+            default: false 
+        },
+        color: {
+            type: String,
+            default: ''
+        },
+        elevation: {
+            type: String,
+            default: '' 
+        },
+        dark: {
+            type: Boolean,
+            default: false
+        },
+        small: {
+            type: Boolean,
+            default: false 
+        },
+        large: {
+            type: Boolean,
+            default: false 
+        }
+    }
+}
+</script>
